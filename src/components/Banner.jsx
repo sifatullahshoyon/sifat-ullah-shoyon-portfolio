875d7ea243d42d550350fc198edb1279985cf5b8
freelancersifat627e <freelancersifat627@gmail.comm>
2024-10-22 00:09:23 +0600

    some code updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,10 @@
+"use client";
 import React from 'react';
 import bannerImg from '@/assets/images/banner/banner1.png';
 import Image from 'next/image';
+import Lottie from 'lottie-react';
+
+import downloadIcon from "../assets/images/icon/download.json";
 
 const Banner = () => {
     return (
@@ -8,19 +12,24 @@ const Banner = () => {
             {/* Content */}
             {/* red Bg Start */}
             <div className='red-bg'>Lorem ipsum dolor sit amet.</div>
+            <div className='skey-bg'>Lorem ipsum dolor sit amet.</div>
             {/* red Bg End */}
-            <div className='z-10'>
+            <div className='z-10 px-10'>
                 <p className='text-[#FFE600] text-5xl font-bold'>Hello,</p>
                 <h1 className='text-[90px] leading-tight text-white text-balance font-bold capitalize my-2'>I&apos;m Sifat <br /> Ullah  Shoyon</h1>
                 <p className='text-[#59C378] text-[32px] font-semibold mb-6'>Front End Developer</p>
                 <p className='mb-8'>My deep passion for programming and curiosity about emerging technologies <br /> continuously drives me to take on new challenges.</p>
-                <div>
-                    <button className='btn btn-primary mr-5'>Resume</button>
-                    <button className='btn btn-success'>Hire Me</button>
+                <div className='flex'>
+                    <div className='flex'>
+                    <button className='btn relative items-center  bg-white rounded-full w-32 mr-5 text-black hover:bg-white'>Resume
+                    <Lottie className='w-10 absolute top-0 right-0 text-white' color='white' animationData={downloadIcon} loop={true} />
+                    </button>
+                    </div>
+                    <button className='btn w-32 rounded-full btn-success text-white'>Hire Me</button>
                 </div>
             </div>
             {/* shape Start */}
-            <div className='bg-shape-one'></div>
+            {/* <div className='bg-shape-one'></div> */}
             {/* shape End */}
             {/* Image */}
             <div className='z-10'>
